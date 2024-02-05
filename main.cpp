cd7ed0167c1d2c41f845a9bb47a6961283645531
Lucas Farias <lukf95@gmail.com>
2024-02-05 23:20:46 +0100

    update

--- main.cpp
+++ main.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
 #include <boost/asio.hpp>
-#include <boost/array.hpp>
+#include <iostream>
+#include <string>
 
 using boost::asio::ip::tcp;
 
 int main() {
     try {
         boost::asio::io_context io_context;
-        tcp::resolver resolver(io_context);
-        const auto endpoints = resolver.resolve("127.0.0.1", "7666");
+
+        std::string server_address = "127.0.0.1";
         tcp::socket socket(io_context);
-        boost::asio::connect(socket, endpoints);
+        tcp::resolver resolver(io_context);
+        boost::asio::connect(socket, resolver.resolve(server_address, "1234"));
 
-        for (;;) {
-            boost::array<char, 128> buf{};
-            boost::system::error_code error;
+        std::cout << "Connected to the server. Enter messages to send:\n";
 
-            const size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-            if (error == boost::asio::error::eof)
-                break;
-            else if (error)
-                throw boost::system::system_error(error);
-
-            std::cout.write(buf.data(), len);
+        std::string message;
+        while (std::getline(std::cin, message)) {
+            message += "\n";
+            boost::asio::write(socket, boost::asio::buffer(message));
+            std::cout << "Message sent. Enter another message:\n";
         }
     } catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << "\n";
